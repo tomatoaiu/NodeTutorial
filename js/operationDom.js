15cda3088b48f21df7d6cbb59326b0890f7cde86
unknown <g031n112@ad.iwate-pu.ac.jp>
2017-12-14 08:59:02 +0000

    add cursin, andaunt and uncle

--- js/operationDom.js
+++ js/operationDom.js
@@ -10,5 +10,23 @@ var text = document.createTextNode("child");
 child.appendChild(text);
 // add class to elemet
 child.className = "aaa";
+
+// clone child
+var uncle = child.cloneNode(true);
+
 // add element under parent
 last.append(child);
+
+// error
+// last.parentNode.insertBefore(child);
+uncle.textContent = "uncle";
+
+var aunt = uncle.cloneNode(true);
+
+// add uncle immediately after last(element)
+last.parentNode.insertBefore(uncle, last.nextSibling);
+
+aunt.innerHTML = "aunt\
+                    <div>aunt's child</div>";
+
+uncle.parentNode.insertBefore(aunt, last.nextSibling);
