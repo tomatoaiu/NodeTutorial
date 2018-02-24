d57a59353d0828f88ffa07fdcb082ec4fa70a946
Shunsuke Toba <boushi_sukaimu@yahoo.co.jp>
2018-02-24 17:05:08 +0900

    refactor: 3baka,js

--- js/3baka.js
+++ js/3baka.js
@@ -1,4 +1,3 @@
-// node js/3baka.js
 // test mocha js/3baka.js
 
 const chai = require(`chai`);
@@ -8,11 +7,9 @@ const notNumber = `Not Number`;
 
 const main = (inputStr, num) => {
     const strArray = stringToArray(inputStr, ` `);
-    console.log(strArray);
-    for (const str of strArray) {
-        console.log(baka(str, num));
-        // test return baka(str, num);
-    }
+    return strArray.map((str) => {
+        return baka(str, num);
+    });
 };
 
 const baka = (valueStr, targetNum) => {
@@ -23,7 +20,7 @@ const baka = (valueStr, targetNum) => {
     if(num === 0) return `${num} は 0 ですよ`;
 
     if(num % targetNum === 0 && isStrIncludeNum(num, targetNum)) {
-        return `${targetNum}で割れて${targetNum}を含んますよ`;
+        return `${targetNum}で割れて${targetNum}を含んでますよ`;
     } else if(num % targetNum === 0){
         return `${num} は${targetNum}で割り切れますよ`;
     } else if(isStrIncludeNum(num, targetNum)){
@@ -60,41 +57,44 @@ const stringToArray = (str, sep) => {
     }
 };
 
-main("", 3);
-main("a", 3);
-main("12", 3);
-main("12 32", 3);
-main("3 4 a 1", 3);
-main("0", 3);
-
-
 describe(`test 1`, () => {
     it(`num is [Not String]`, () => {
-        assert.equal(baka("", 3), `Not String`);
+        assert.equal(main("", 3)[0], `Not String`);
     });
 });
 
 describe(`test 2`, () => {
     it(`num is [Not Number]`, () => {
-        assert.equal(baka("a", 3), `Not Number`);
+        assert.equal(main("a", 3)[0], `Not Number`);
     });
 });
 
 describe(`test 3`, () => {
     it(`num is [12 は3で割り切れますよ]`, () => {
-        assert.equal(baka("12", 3), `12 は3で割り切れますよ`);
+        assert.equal(main("12", 3)[0], `12 は3で割り切れますよ`);
     });
 });
 
 describe(`test 4`, () => {
     it(`num is [12 は3で割り切れますよ], num is [32 は3を含んでいますよ]`, () => {
-        assert.equal(baka("12", 3), `12 は3で割り切れますよ`);
-        assert.equal(baka("32", 3), `32 は3を含んでいますよ`);
+        const result = main("12 32", 3);
+        assert.equal(result[0], `12 は3で割り切れますよ`);
+        assert.equal(result[1], `32 は3を含んでいますよ`);
+    });
+});
+
+describe(`test 5`, () => {
+    it(`3 is [3で割れて3を含んでますよ], 4 is [以外], a is [Not Number], 1 is [以外]`, () => {
+        const result = main("3 4 a 1", 3);
+        assert.equal(result[0], `3で割れて3を含んでますよ`);
+        assert.equal(result[1], `以外`);
+        assert.equal(result[2], `Not Number`);
+        assert.equal(result[3], `以外`);
     });
 });
 
 describe(`test 6`, () => {
     it(`num is [0 は 0 ですよ]`, () => {
-        assert.equal(baka("0", 3), `0 は 0 ですよ`);
+        assert.equal(main("0", 3)[0], `0 は 0 ですよ`);
     });
 });
